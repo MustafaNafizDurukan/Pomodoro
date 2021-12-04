719e7a07d96b912325cfbeae8071f755364822a6
Mustafa Nafiz Durukan <mustafa@binalyze.com>
2021-12-04 07:22:32 +0300

    Message list added

---
+++ pkg/list/message.go
@@ -0,0 +1,27 @@
+package list
+
+import "math/rand"
+
+var messages = []string{
+	"Don't look at me",
+	"Leave me alone!",
+	"Don't give up!",
+	"You are almost there!",
+	"Go back to your work!",
+	"Hang in there!",
+	"Don't be distracted!",
+	"You can do it!",
+	"Stay focused!",
+	"Stop surfing the web!",
+	"What you plant now, you will harvest later.",
+	"Keep up the good work!",
+	"Don’t let fear of failure put you off!",
+	"Don’t quit yet!",
+	"Keep going",
+}
+
+// Message returns random message from messages list
+func Message() string {
+	randNum := rand.Intn(len(messages) - 1)
+	return messages[randNum]
+}
